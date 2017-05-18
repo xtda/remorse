70f6310f1a8eb3828e91d5831192e1e46697d2ee
Andrew Jackson <andrew@disvelop.net>
2017-05-18 18:39:02 +1000

    reworked javascript and updated menubar

--- config/routes.rb
+++ config/routes.rb
@@ -2,17 +2,17 @@ Rails.application.routes.draw do
 
   get 'page/new', to: 'posts#new', as: 'posts'
   get 'page/:id', to: 'posts#show', as: 'post'
-  post 'page/new', to: 'posts#create'
+  post 'admin/newpost', to: 'posts#create'
   get 'page/:id/edit', to: 'posts#edit', as: 'post_edit'
   patch 'page/:id', to: 'posts#update'
-  get 'page', to: 'posts#index'
+  get '/admin/page', to: 'posts#index', as: 'page'
   delete 'page/:id', to: 'posts#destroy'
   get '/admin/feature', to: 'posts#feature_edit', as: 'feature'
 
   get '/apply', to: 'playerapplication#new'
   get 'application/:id', to: 'playerapplication#show', as: 'playerapplication'
   delete 'application/:id', to: 'playerapplication#destroy'
-  get 'applications', to: 'playerapplication#index'
+  get '/admin/applications', to: 'playerapplication#index', as: 'applications'
   post 'apply', to: 'playerapplication#create', as: 'playerapplications'
 
   get '/admin/recruitment', to: 'recruitment#index', as: 'recruitment'
@@ -23,15 +23,13 @@ Rails.application.routes.draw do
     root to: 'users#index'
   end
 
-  #constraints subdomain: 'api' do
-    scope module: 'api' do
-      namespace :v1 do
-        get 'health', to: 'health#index'
-        get 'app/:char/:realm', to: 'guildapp#show'
-        get 'recuriting', to: 'guildapp#recruiting'
-      end
+  scope module: 'api' do
+    namespace :v1 do
+      get 'health', to: 'health#index'
+      get 'app/:char/:realm', to: 'guildapp#show'
+      get 'recuriting', to: 'guildapp#recruiting'
     end
-  #end
+  end
 
   root 'home#index'
   get    '/login',   to: 'sessions#new'
