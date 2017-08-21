11b3ee518618015cea58ce666be4fbacfd5768db
Andrew Jackson <andrew@disvelop.net>
2017-08-21 22:13:28 +1000

    updated roster output

--- app/controllers/roster_controller.rb
+++ app/controllers/roster_controller.rb
@@ -1,12 +1,19 @@
 class RosterController < ApplicationController
   include WowHelper
-
+  before_action :authenticate_admin, only: [:new]
   def index
     @roster = Roster.where(hidden: false).order('role', 'player_name')
   end
 
   def new
     guild_roster('frostmourne', 'remorse', 5)
-    render plain: 'OK 200'
+    redirect_to roster_path
+  end
+
+  private
+
+  def authenticate_admin
+    return redirect_to root_url unless logged_in?
+    redirect_to root_url unless current_user.admin? || current_user.dev?
   end
 end
